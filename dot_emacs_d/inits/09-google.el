2fbef512cf0547dc6dde5761d60649c93bb52cdc
miyazaki <miyazaki_masayasu_f5@lab.ntt.co.jp>
2016-01-06 16:25:56 +0900

    modified el

--- dot_emacs_d/inits/09-google.el
+++ dot_emacs_d/inits/09-google.el
@@ -1,25 +1,3 @@
-(require 'google-translate)
-(global-set-key "\C-ct" 'google-translate-at-point)
-(global-set-key "\C-cT" 'google-translate-query-translate)
-
-;; 翻訳のデフォルト値を設定（en -> ja）
-(custom-set-variables
-  '(google-translate-default-source-language "en")
-  '(google-translate-default-target-language "ja"))
-
-(global-set-key (kbd "C-x T") '(lambda ()
-				 (interactive)
-				 (let ((google-translate-default-source-language "ja")
-				       (google-translate-default-target-language "en"))
-				   (google-translate-at-point))))
-
-
-;; popwin.el
-(require 'popwin)
-
-(setq display-buffer-function 'popwin:display-buffer)
-;; ポップアップを画面下に表示
-(setq popwin:popup-window-position 'bottom)
-
-;; google-translate.elの翻訳バッファをポップアップで表示させる
-(push '("*Google Translate*") popwin:special-display-config)
+(add-to-list 'load-path "{{gtran-emacs}}")
+(require 'gtran)
+(global-set-key "\C-ct" 'pop-gtran)
